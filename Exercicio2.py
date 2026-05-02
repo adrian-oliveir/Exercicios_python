acc8638c5bdb8730d420472ed223318fd42a3595
Adrian Renan de Oliveira amorim <adrian.work6n@gmail.com>
2026-05-01 23:23:32 -0300

    Create Exercicio2.py

---
+++ Exercicio2.py
@@ -0,0 +1,5 @@
+selecao_carro = int(input("Selecione quanto carros você deseja alugar: "))
+selecao_dias = int(input("Selecione por quantos dias você deseja alugar: "))
+multiplicacao = selecao_carro * selecao_dias
+multiplicacao2 = multiplicacao * 100
+print("O valor que você vai pgar é:", multiplicacao2)
