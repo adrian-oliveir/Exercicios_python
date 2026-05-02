8d3f813f34e687fbbabd0e3981c6464c607c6bc3
Adrian Renan de Oliveira amorim <adrian.work6n@gmail.com>
2026-05-01 23:06:13 -0300

    Calcular sua idade em meses
    Esse exercício busca entender sua idade atual e calcular em meses.

---
+++ Exercicio1.py
@@ -0,0 +1,5 @@
+ano_nascimento = int(input("digite seu ano de nascimento:"))
+ano_atual = int(input("digite o ano atual:"))
+idade = ano_atual - ano_nascimento
+idade_meses = idade * 12
+print("Essa é a sua idade:", idade, "e essa é a sua idade em meses:", idade_meses)
